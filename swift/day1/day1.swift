94284e688069d4c4e8be4c24c219d5bd2fdeed22
Evan Freeze <evanfreeze@gmail.com>
2020-12-06 23:13:45 +0000

    fixed day 1

--- swift/day1/day1.swift
+++ swift/day1/day1.swift
@@ -7,12 +7,13 @@ guard let rawInput = try? String(contentsOfFile: "./input.txt", encoding: .utf8)
 let input = rawInput
     .trimmingCharacters(in: .newlines)
     .components(separatedBy: .newlines)
+    .map({ Int($0)! })
 
 func calculateFuel(mass: Int) -> Int {
     Int((Double(mass) / 3).rounded(.down) - 2)
 }
 
-let fuels = input.map({ calculateFuel($0) })
-let result = input.reduce(fuels, +)
+let fuels = input.map({ calculateFuel(mass: $0) })
+let result = fuels.reduce(0, +)
 
 print("PART 1: Total fuel requirement is \(result)")
