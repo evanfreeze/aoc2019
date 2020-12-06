3cf42b658c64105d0fa8f505a9163b975acfea78
Evan Freeze <evanfreeze@gmail.com>
2020-12-06 23:31:48 +0000

    day 1 part 2

--- swift/day1/day1.swift
+++ swift/day1/day1.swift
@@ -9,6 +9,8 @@ let input = rawInput
     .components(separatedBy: .newlines)
     .map({ Int($0)! })
 
+// PART 1
+
 func calculateFuel(mass: Int) -> Int {
     Int((Double(mass) / 3).rounded(.down) - 2)
 }
@@ -17,3 +19,25 @@ let fuels = input.map({ calculateFuel(mass: $0) })
 let result = fuels.reduce(0, +)
 
 print("PART 1: Total fuel requirement is \(result)")
+
+// PART 2
+
+func calculateFuelWithFuel(mass: Int) -> Int {
+    var currentMass = mass
+    var fuel = 0
+    
+    while currentMass >= 0 {
+        let currentFuel = Int((Double(currentMass) / 3).rounded(.down) - 2)
+        if currentFuel > 0 {
+            fuel += currentFuel
+        }
+        currentMass = currentFuel
+    }
+    
+    return fuel
+}
+
+let fuelsP2 = input.map({ calculateFuelWithFuel(mass: $0) })
+let resultP2 = fuelsP2.reduce(0, +)
+
+print("PART 2: Total fuel requirement is \(resultP2)")
